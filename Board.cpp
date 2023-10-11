b0f0195d8652967ee44ddfd4b0cc2e431140b8fb
Luna Gal <lunagal@George.local>
2023-10-11 12:51:38 -0400

    empty classes

---
+++ Board.cpp
@@ -0,0 +1,12 @@
+class Board
+{
+	int size;
+	char grid[size][size];
+
+	void print_board() {
+		// Display the board
+	}
+	void move_neutral(int row, int col, char player) {
+		// Move neutral piece to the specified spot with correct player
+	}
+};
