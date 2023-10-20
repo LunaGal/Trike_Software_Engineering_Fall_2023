d1def6f36d1e9731e5d92be21783df7fd0e1a899
Forrest Curry <ecurry25@wooster.edu>
2023-10-20 12:37:42 -0400

    Game function for starting place of neutral piece

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ int main(int argc, char const *argv[])
 	printf("Nothing yet\n");
 	Board test_board = Board(3);
 	test_board.print_board();
+
 	Game test_game;
+	
+	test_game.request_starting_pos();
+
+
 	return 0;
 }
