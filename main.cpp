a5f3a76e869042316c5a323fca457bc558169f75
Akhmadillo Mamirov <105142060+akhmadmamirov@users.noreply.github.com>
2023-10-20 12:53:25 -0400

    Update main.cpp
    Cleaning main func

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ int main(int argc, char const *argv[])
 {
 	printf("Nothing yet\n");
 	Board test_board = Board(3);
-
+	
 	test_board.print_board();
 
 
-    test_board.move_neutral(0, 1, 'A');
-    test_board.print_board();
-    test_board.move_neutral(1, 1, 'B');
-    test_board.print_board();
-    test_board.move_neutral(2, 0, 'A');
-    test_board.print_board();
+	test_board.move_neutral(0, 1, 'A');
+	test_board.print_board();
+	test_board.move_neutral(1, 1, 'B');
+	test_board.print_board();
+	test_board.move_neutral(2, 0, 'A');
+	test_board.print_board();
 
 	Game test_game;
 	
