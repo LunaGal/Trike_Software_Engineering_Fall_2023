77f5c9dc507c45baf1fd7661823f322131a172d3
Luna Gal <lunagal@George.local>
2023-10-25 12:06:19 -0400

    added to the skeleton

--- Board.cpp
+++ Board.cpp
@@ -33,6 +33,11 @@ void Board::move_neutral(int row, int col, char player) {
 
 }
 
+Board::check_legal_movement(int new_xpos, int new_ypos) {
+	// checks if you can go to new_xpos, new_ypos on the board
+	// returns true if you can
+}
+
 int Board::get_size(){
 	return size;
 }
