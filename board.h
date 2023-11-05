c55e7cbd27111f37e91dad4be957409885ed064d
Forrest Curry <ecurry25@wooster.edu>
2023-11-05 14:50:43 -0500

    game over method

--- board.h
+++ board.h
@@ -26,6 +26,7 @@ class Board {
 		bool check_legal_diagonal(int new_xpos, int new_ypos);
         bool check_bounds(int test_x, int test_y);
         char get_winner();
+		bool game_over();
 
 		Board();
 		Board(int new_size);
